de104042c057ffd6310f14e92e20dd656c075c46
JNPRZ2000 <3143611380710@ingenieria.usac.edu.gt>
2021-05-03 13:47:20 -0600

    Update views.py

--- hospital/appweb/views.py
+++ hospital/appweb/views.py
@@ -3,32 +3,12 @@ from appweb.clases.objetos import AdminWeb, Doctor, Nurse, Patient, Medicine, Re
 import json
 
 # Create your views here.
-# Create an admin and list for different type of users
-doc1 = Doctor("Juan","Pérez","06/01/2000","M","jn612perez","123456789","Cirujano","49557946",0)
-doc2 = Doctor("Carola","Aguilón","15/05/1999","F","cati15","123456789","Cardiologo","12345678",1)
-doc3 = Doctor("Alicia","Shar","04/11/2001","F","amshar","123456789","Dentista","12345678",2)
-doc4 = Doctor("Pablo","Búc","01/06/2000","M","pablobuc","123456789","Anestesista","12345678",3)
-
-enf1 = Nurse("Milagros","Begazo","07/07/2000","F","milagrosbegazo","123456789","12345678",0)
-enf2 = Nurse("Mariela","Torres","13/02/2000","F","marielatorres","123456789","12345678",1)
-enf3 = Nurse("Stephania","lopez","12/05/2000","F","tefylopez","123456789","12345678",2)
-enf4 = Nurse("Marlene","Arriaga","26/08/2001","F","marlenearriaga","123456789","12345678",3)
-
-pac1 = Patient("Fernanda","Monzón","30/06/1998","F","fernandamonzon","123456789","12345678",0)
-pac2 = Patient("Ivan","Rodriguez","26/12/1996","M","ivanrodriguez","123456789","12345678",1)
-pac3 = Patient("Hugo","García","19/02/1999","M","hugogarcia","123456789","12345678",2)
-pac4 = Patient("Jhonson","Avery","30/06/1998","M","jhonsonavery","123456789","12345678",3)
-
-med1 = Medicine("medicina1","170","primera descripcion","50")
-med2 = Medicine("medicina2","200","segunda descripcion","90")
-med3 = Medicine("medicina3","90","tercera descripcion","70")
-med4 = Medicine("medicina4","180","cuarta descripcion","85")
 
 admin = AdminWeb()
-doctors = [doc1,doc2,doc3,doc4]
-nurses = [enf1,enf2,enf3,enf4]
-patients = [pac1,pac2,pac3,pac4]
-medicines = [med1,med2,med3,med4]
+doctors = []
+nurses = []
+patients = []
+medicines = []
 recetas = []
 
 
@@ -107,9 +87,6 @@ def registro(request):
             username = datos["usuario"]
             contrasena = datos["contraseña"]
             patients.append(Patient(nombre,apellido,fecha,genero,username,contrasena,telefono,len(patients)))
-            for i in range(len(patients)):
-                print(i)
-                print(patients[i].usuario)
             return render(request, "registro.html")
     else:
         return render(request, "registro.html")
